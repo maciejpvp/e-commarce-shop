06480fb49b8c8e092be09a77a74da42f75268316
maciejpvp <oskar.pyrzak@gmail.com>
2026-02-14 20:37:20 +0100

    feat(api): add cognito authorizer

--- infra/modules/api_gateway/api_gateway.tf
+++ infra/modules/api_gateway/api_gateway.tf
@@ -36,22 +36,6 @@ resource "aws_api_gateway_stage" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   stage_name    = "prod"
 
-  # access_log_settings {
-  #   destination_arn = aws_cloudwatch_log_group.api_gw.arn
-  #   format = jsonencode({
-  #     requestId      = "$context.requestId"
-  #     ip             = "$context.identity.sourceIp"
-  #     caller         = "$context.identity.caller"
-  #     user           = "$context.identity.user"
-  #     requestTime    = "$context.requestTime"
-  #     httpMethod     = "$context.httpMethod"
-  #     resourcePath   = "$context.resourcePath"
-  #     status         = "$context.status"
-  #     protocol       = "$context.protocol"
-  #     responseLength = "$context.responseLength"
-  #   })
-  # }
-
   tags = {
     Environment = var.Environment
     Terraform   = "true"
